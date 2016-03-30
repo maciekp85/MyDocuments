56bcba17e37b1f7f712325d5cf5de8c0ee80a706
Maciej Piotrowski <nishi@wimiip.pl>
2016-03-30 20:41:14 +0200

    Chapter 5: collections and own types

---
+++ r05/src/main/java/com/apress/isf/java/service/SearchEngine.java
@@ -0,0 +1,14 @@
+package com.apress.isf.java.service;
+
+import com.apress.isf.java.model.Document;
+import com.apress.isf.java.model.Type;
+
+import java.util.List;
+
+/**
+ * Created by nishi on 2016-03-29.
+ */
+public interface SearchEngine {
+    public List<Document> findByType(Type type);
+    public List<Document> listAll();
+}
